0c47a9686cf4de7b4a66d1f446d8fa75fc858024
Raia Quitoriano <rsquitoriano@gmail.com>
2024-02-27 13:41:08 +0800

    test

--- app.js
+++ app.js
@@ -9,6 +9,24 @@ var cors = require('cors')
 const mongoose = require('mongoose');
 
 const mongoString = process.env.DATABASE_URL
+var MongoClient = require("mongodb").MongoClient;  
+app.get("/users", function() {  
+  MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {  
+    if (err) next  
+    db  
+      .collection("users")  
+      .find()  
+      .toArray(function(err, result) {  
+        if (err) throw err;  
+
+        res.json(result)  
+      });  
+  });  
+});
+
+app.listen(3000,function(){  
+    console.log('Express app start on port 3000')  
+});
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
